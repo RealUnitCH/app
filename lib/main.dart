7fa5c2569ea6bca9e10c93a7527788029036b25f
TuanLamNguyen <xnguyen.lam@gmail.com>
2025-12-02 14:28:06 +0100

    fix: make status bar and app bar consistent.

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,12 @@ import 'package:go_router/go_router.dart';
 
 Future<void> main() async {
   // try {
-    WidgetsFlutterBinding.ensureInitialized();
-    await fuckFirebase();
-    final databaseKey = await setupEssentials();
-    await finishSetup(databaseKey);
+  WidgetsFlutterBinding.ensureInitialized();
+  await fuckFirebase();
+  final databaseKey = await setupEssentials();
+  await finishSetup(databaseKey);
 
-    runApp(const WalletApp());
+  runApp(const WalletApp());
   // } catch (e) {
   //   runApp(
   //     MaterialApp(
@@ -80,8 +80,7 @@ class _WalletAppState extends State<WalletApp> {
   void _onDetached() => debugPrint('detached');
 
   void _onResumed() {
-    getIt<BalanceService>()
-        .updateERC20Balances(getIt<AppStore>().primaryAddress);
+    getIt<BalanceService>().updateERC20Balances(getIt<AppStore>().primaryAddress);
   }
 
   void _onInactive() => developer.log('inactive', name: 'AppLifecycleListener');
@@ -101,7 +100,7 @@ class _WalletAppState extends State<WalletApp> {
         child: BlocBuilder<SettingsBloc, SettingsState>(
           builder: (context, state) => MaterialApp.router(
             debugShowCheckedModeBanner: false,
-            theme: darkTheme,
+            theme: realUnitTheme,
             supportedLocales: S.delegate.supportedLocales,
             localizationsDelegates: const [
               S.delegate,
@@ -114,8 +113,7 @@ class _WalletAppState extends State<WalletApp> {
             builder: (context, child) => BlocListener<HomeBloc, HomeState>(
               listener: (context, state) {
                 if (!state.isLoadingWallet) {
-                  getIt<GoRouter>()
-                      .go(state.openWallet != null ? '/dashboard' : '/welcome');
+                  getIt<GoRouter>().go(state.openWallet != null ? '/dashboard' : '/welcome');
                 }
               },
               child: child,
--- lib/styles/themes.dart
+++ lib/styles/themes.dart
@@ -1,9 +1,21 @@
-import 'package:realunit_wallet/styles/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:realunit_wallet/styles/colors.dart';
 
-ThemeData get darkTheme => ThemeData(
-  fontFamily: "Open Sans",
-  colorScheme: ColorScheme.fromSeed(seedColor: RealUnitColors.realUnitBlue),
-  useMaterial3: true,
-  scaffoldBackgroundColor: RealUnitColors.neutral100,
-);
+ThemeData get realUnitTheme => ThemeData(
+      fontFamily: "Open Sans",
+      colorScheme: ColorScheme.fromSeed(seedColor: RealUnitColors.realUnitBlue),
+      useMaterial3: true,
+      scaffoldBackgroundColor: RealUnitColors.neutral100,
+      appBarTheme: AppBarTheme(
+        scrolledUnderElevation: 0.0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: RealUnitColors.realUnitBlack,
+        iconTheme: IconThemeData(color: RealUnitColors.realUnitBlack),
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.dark, // For Android
+          statusBarBrightness: Brightness.light, // For iOS
+        ),
+      ),
+    );
