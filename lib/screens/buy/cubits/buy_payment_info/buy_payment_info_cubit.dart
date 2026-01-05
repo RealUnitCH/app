39627a05db30c3ad538d9c851d462952ed6c1926
TuanLamNguyen <xnguyen.lam@gmail.com>
2026-01-05 10:46:38 +0100

    refactor: use CancelableOperation instead of synchronized.

--- lib/screens/buy/cubits/buy_payment_info/buy_payment_info_cubit.dart
+++ lib/screens/buy/cubits/buy_payment_info/buy_payment_info_cubit.dart
@@ -1,5 +1,6 @@
 import 'dart:developer' as developer;
 
+import 'package:async/async.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:realunit_wallet/packages/service/dfx/exceptions/payment/buy_exceptions.dart';
@@ -7,35 +8,51 @@ import 'package:realunit_wallet/packages/service/dfx/models/payment/buy_payment_
 import 'package:realunit_wallet/packages/service/dfx/models/payment/buy_payment_info_error.dart';
 import 'package:realunit_wallet/packages/service/dfx/real_unit_buy_payment_info_service.dart';
 import 'package:realunit_wallet/styles/currency.dart';
-import 'package:synchronized/synchronized.dart';
 
 part 'buy_payment_info_state.dart';
 
 class BuyPaymentInfoCubit extends Cubit<BuyPaymentInfoState> {
   final RealUnitBuyPaymentInfoService _buyPaymentInfoService;
-  final Lock _lock = Lock();
+  CancelableOperation<BuyPaymentInfoState>? _completer;
 
   BuyPaymentInfoCubit(this._buyPaymentInfoService) : super(BuyPaymentInfoInitial());
 
   Future<void> getPaymentInfo({String amount = '300', Currency currency = Currency.chf}) async {
-    await _lock.synchronized(() async {
-      try {
-        if (state is! BuyPaymentInfoSuccess) {
-          emit(BuyPaymentInfoLoading());
-        }
-        final paymentInfo = await _buyPaymentInfoService.getPaymentInfo(
-          double.parse(amount).round(),
-          currency: currency,
-        );
-        emit(BuyPaymentInfoSuccess(paymentInfo));
-      } on KycLevelRequiredException {
-        emit(BuyPaymentInfoFailure(BuyPaymentInfoError.kycRequired));
-      } on RegistrationRequiredException {
-        emit(BuyPaymentInfoFailure(BuyPaymentInfoError.registrationRequired));
-      } catch (e) {
-        developer.log(e.toString());
-        emit(BuyPaymentInfoFailure(BuyPaymentInfoError.unknown));
-      }
-    });
+    await _completer?.cancel();
+
+    if (state is! BuyPaymentInfoSuccess) {
+      emit(BuyPaymentInfoLoading());
+    }
+
+    _completer = CancelableOperation.fromFuture(
+      _runGetPaymentInfo(amount, currency),
+    );
+
+    final newState = await _completer!.value;
+    emit(newState);
+  }
+
+  Future<BuyPaymentInfoState> _runGetPaymentInfo(String amount, Currency currency) async {
+    try {
+      final paymentInfo = await _buyPaymentInfoService.getPaymentInfo(
+        double.parse(amount).round(),
+        currency: currency,
+      );
+
+      return BuyPaymentInfoSuccess(paymentInfo);
+    } on KycLevelRequiredException {
+      return BuyPaymentInfoFailure(BuyPaymentInfoError.kycRequired);
+    } on RegistrationRequiredException {
+      return BuyPaymentInfoFailure(BuyPaymentInfoError.registrationRequired);
+    } catch (e) {
+      developer.log(e.toString());
+      return BuyPaymentInfoFailure(BuyPaymentInfoError.unknown);
+    }
+  }
+
+  @override
+  Future<void> close() async {
+    await _completer?.cancel();
+    return super.close();
   }
 }
