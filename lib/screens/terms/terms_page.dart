29cc6d1d58ce6012ca0c17b2f0d12cd20f5e99bf
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2026-02-19 14:56:12 +0100

    fix: improve terms screen gradient, register splash asset, use Sie form

--- lib/screens/terms/terms_page.dart
+++ lib/screens/terms/terms_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:realunit_wallet/generated/i18n.dart';
 import 'package:realunit_wallet/screens/home/bloc/home_bloc.dart';
+import 'package:realunit_wallet/styles/colors.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class TermsPage extends StatelessWidget {
@@ -22,64 +23,84 @@ class TermsPage extends StatelessWidget {
             'assets/images/splash/splash_background.png',
             fit: BoxFit.cover,
           ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Column(
-                children: [
-                  const Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 16.0),
-                    child: RichText(
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                        style: const TextStyle(
-                          fontSize: 14,
-                          height: 20 / 14,
-                          color: Colors.white70,
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  stops: [0.0, 0.4, 1.0],
+                  colors: [
+                    Colors.transparent,
+                    Colors.white,
+                    Colors.white,
+                  ],
+                ),
+              ),
+              child: SafeArea(
+                top: false,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 120.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            style: const TextStyle(
+                              fontSize: 14,
+                              height: 20 / 14,
+                              color: RealUnitColors.neutral500,
+                            ),
+                            children: [
+                              TextSpan(text: '${s.softwareTermsText} '),
+                              TextSpan(
+                                text: s.termsAndConditions,
+                                style: const TextStyle(
+                                  color: RealUnitColors.realUnitBlue,
+                                  decoration: TextDecoration.underline,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () => _launchUrl(
+                                    'https://realunit.ch/app/nutzungsbedingungen',
+                                  ),
+                              ),
+                              TextSpan(text: ' ${s.softwareTermsAnd} '),
+                              TextSpan(
+                                text: s.softwareTermsDisclaimer,
+                                style: const TextStyle(
+                                  color: RealUnitColors.realUnitBlue,
+                                  decoration: TextDecoration.underline,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () => _launchUrl(
+                                    'https://realunit.ch/app/haftungsausschluss',
+                                  ),
+                              ),
+                              const TextSpan(text: '.'),
+                            ],
+                          ),
                         ),
-                        children: [
-                          TextSpan(text: '${s.softwareTermsText} '),
-                          TextSpan(
-                            text: s.termsAndConditions,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              decoration: TextDecoration.underline,
-                            ),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () => _launchUrl(
-                                'https://realunit.ch/app/nutzungsbedingungen',
-                              ),
-                          ),
-                          TextSpan(text: ' ${s.softwareTermsAnd} '),
-                          TextSpan(
-                            text: s.softwareTermsDisclaimer,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              decoration: TextDecoration.underline,
-                            ),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () => _launchUrl(
-                                'https://realunit.ch/app/haftungsausschluss',
-                              ),
-                          ),
-                          const TextSpan(text: '.'),
-                        ],
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: FilledButton(
-                        onPressed: () =>
-                            context.read<HomeBloc>().add(const AcceptSoftwareTermsEvent()),
-                        child: Text(s.next),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 20.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: FilledButton(
+                            onPressed: () =>
+                                context.read<HomeBloc>().add(const AcceptSoftwareTermsEvent()),
+                            child: Text(s.next),
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
