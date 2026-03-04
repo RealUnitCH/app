cef5a426b34ef6ab2a6c9e02e8047d457802611b
TuanLamNguyen <xnguyen.lam@gmail.com>
2026-03-04 11:32:34 +0100

    test: SettingsWalletAddressPage tests.

---
+++ test/screens/settings_wallet_address/settings_wallet_address_page_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:realunit_wallet/generated/i18n.dart';
+import 'package:realunit_wallet/packages/service/app_store.dart';
+import 'package:realunit_wallet/screens/receive/widgets/qr_address_widget.dart';
+import 'package:realunit_wallet/screens/settings_wallet_address/settings_wallet_address_page.dart';
+
+import '../../helper/helper.dart';
+
+class MockAppStore extends Mock implements AppStore {}
+
+void main() {
+  final AppStore appStore = MockAppStore();
+
+  setUp(() {
+    when(() => appStore.primaryAddress).thenReturn('0x938115b533a0b746428361760a6972dfd06d984a');
+  });
+
+  void setupDependencyInjection() {
+    final getIt = GetIt.instance;
+    getIt.registerSingleton<AppStore>(appStore);
+  }
+
+  setUpAll(() {
+    setupDependencyInjection();
+  });
+
+  group('$SettingsWalletAddressPage', () {
+    testWidgets('renders initially correctly', (tester) async {
+      await tester.pumpApp(const SettingsWalletAddressPage());
+
+      expect(find.byType(SvgPicture), findsOne);
+      expect(find.byType(QRAddressWidget), findsOne);
+      expect(
+        find.byWidgetPredicate(
+          (widget) => widget is Text && widget.data == S.current.walletAddressDisclaimer,
+        ),
+        findsOne,
+      );
+    });
+  });
+}
