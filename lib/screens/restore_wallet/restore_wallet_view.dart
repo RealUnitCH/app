4d77273d7ee65bb3ba393e7fa832d357ef64b4bf
Lam Nguyen <32935491+xlamn@users.noreply.github.com>
2026-03-05 15:07:01 +0100

    feat: design changes (#122)
    - update texts
    - update illustrations

--- lib/screens/restore_wallet/restore_wallet_view.dart
+++ lib/screens/restore_wallet/restore_wallet_view.dart
@@ -20,98 +20,97 @@ class RestoreWalletView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MultiBlocListener(
-        listeners: [
-          BlocListener<RestoreWalletCubit, RestoreWalletState>(
-            listenWhen: (previous, current) => previous.wallet != current.wallet,
-            listener: (context, state) async {
-              if (state.wallet != null) {
-                await Future.delayed(const Duration(seconds: 2));
-                if (context.mounted) context.read<HomeBloc>().add(LoadWalletEvent(state.wallet!));
-              }
-            },
-          ),
-          BlocListener<ValidateSeedCubit, ValidateSeedState>(
-            listener: (context, seedState) {
-              if (seedState == ValidateSeedState.valid) {
-                context.read<RestoreWalletCubit>().restoreWallet(_controllers.seed);
-              }
-            },
-          ),
-        ],
-        child: Scaffold(
-          backgroundColor: RealUnitColors.brand700,
-          appBar: AppBar(),
-          body: SafeArea(
-            child: GestureDetector(
-              onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-              behavior: HitTestBehavior.opaque,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: LayoutBuilder(
-                  builder: (context, constraint) {
-                    return SingleChildScrollView(
-                      child: ConstrainedBox(
-                        constraints: BoxConstraints(minHeight: constraint.maxHeight),
-                        child: IntrinsicHeight(
-                          child: Column(
-                            children: <Widget>[
-                              SvgPicture.asset(
-                                'assets/images/restore_wallet.svg',
-                                height: 124,
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              Text(
-                                S.of(context).restoreWallet,
-                                style: const TextStyle(
-                                  fontSize: 26,
-                                  color: RealUnitColors.realUnitBlack,
-                                  fontWeight: FontWeight.bold,
-                                  height: 30 / 26,
-                                  letterSpacing: -0.52,
+    listeners: [
+      BlocListener<RestoreWalletCubit, RestoreWalletState>(
+        listenWhen: (previous, current) => previous.wallet != current.wallet,
+        listener: (context, state) async {
+          if (state.wallet != null) {
+            await Future.delayed(const Duration(seconds: 2));
+            if (context.mounted) context.read<HomeBloc>().add(LoadWalletEvent(state.wallet!));
+          }
+        },
+      ),
+      BlocListener<ValidateSeedCubit, ValidateSeedState>(
+        listener: (context, seedState) {
+          if (seedState == ValidateSeedState.valid) {
+            context.read<RestoreWalletCubit>().restoreWallet(_controllers.seed);
+          }
+        },
+      ),
+    ],
+    child: Scaffold(
+      backgroundColor: RealUnitColors.brand700,
+      appBar: AppBar(),
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+          behavior: HitTestBehavior.opaque,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: LayoutBuilder(
+              builder: (context, constraint) {
+                return SingleChildScrollView(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(minHeight: constraint.maxHeight),
+                    child: IntrinsicHeight(
+                      child: Column(
+                        children: <Widget>[
+                          SvgPicture.asset(
+                            'assets/images/illustrations/restore_wallet.svg',
+                            height: 124,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            S.of(context).restoreWallet,
+                            style: const TextStyle(
+                              fontSize: 26,
+                              color: RealUnitColors.realUnitBlack,
+                              fontWeight: FontWeight.bold,
+                              height: 30 / 26,
+                              letterSpacing: -0.52,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          Row(
+                            spacing: 8.0,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const RecoveryKeyIcon(size: 20, color: RealUnitColors.realUnitBlue),
+                              Expanded(
+                                child: TextSubstringHighlighting(
+                                  text: S.of(context).restoreWalletFromSeedDescription,
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    color: RealUnitColors.neutral500,
+                                  ),
+                                  highlightedText: '12 ${S.of(context).recoveryWords}',
                                 ),
-                              ),
-                              const SizedBox(
-                                height: 40,
-                              ),
-                              Row(
-                                spacing: 8.0,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const RecoveryKeyIcon(
-                                      size: 20, color: RealUnitColors.realUnitBlue),
-                                  Expanded(
-                                    child: TextSubstringHighlighting(
-                                      text: S.of(context).restoreWalletFromSeedDescription,
-                                      style: const TextStyle(
-                                        fontSize: 15,
-                                        color: RealUnitColors.neutral500,
-                                      ),
-                                      highlightedText: '12 ${S.of(context).recoveryWords}',
-                                    ),
-                                  )
-                                ],
-                              ),
-                              const SizedBox(height: 16),
-                              RestoreWalletInputField(
-                                controllers: _controllers,
-                                focusNodes: _focusNodes,
-                              ),
-                              const Spacer(),
-                              RestoreWalletButton(
-                                controllers: _controllers,
                               ),
                             ],
                           ),
-                        ),
+                          const SizedBox(height: 16),
+                          RestoreWalletInputField(
+                            controllers: _controllers,
+                            focusNodes: _focusNodes,
+                          ),
+                          const Spacer(),
+                          RestoreWalletButton(
+                            controllers: _controllers,
+                          ),
+                        ],
                       ),
-                    );
-                  },
-                ),
-              ),
+                    ),
+                  ),
+                );
+              },
             ),
           ),
         ),
-      );
+      ),
+    ),
+  );
 }
