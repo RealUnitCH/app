09da9e3c72b4e60d6acd450741ae60a6c3c6d72c
TuanLamNguyen <xnguyen.lam@gmail.com>
2026-03-05 15:49:32 +0100

    refactor: unify code.

--- lib/screens/legal/steps/legal_documents_step.dart
+++ lib/screens/legal/steps/legal_documents_step.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:realunit_wallet/generated/i18n.dart';
-import 'package:realunit_wallet/screens/legal/subpages/legal_document_page.dart';
+import 'package:realunit_wallet/packages/config/legal_documents_config.dart';
 import 'package:realunit_wallet/screens/legal/widgets/legal_document_button.dart';
 import 'package:realunit_wallet/screens/legal/widgets/legal_step_styles.dart';
 
@@ -30,114 +30,15 @@ class LegalDocumentsStep extends StatelessWidget {
         ),
         Column(
           spacing: 12.0,
-          children: [
-            LegalDocumentButton(
-              leadingIcon: Icons.shield_outlined,
-              title: s.legalDisclaimerCheckboxPrivacyPolicy,
-              onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => LegalDocumentPage(
-                    params: LegalDocumentParams(
-                      title: s.legalDisclaimerCheckboxPrivacyPolicy,
-                      assetBaseName: 'privacy_policy',
-                    ),
-                  ),
+          children: LegalDocumentsConfig.allDocuments
+              .map(
+                (config) => LegalDocumentButton(
+                  leadingIcon: config.icon,
+                  title: config.title(context),
+                  onTap: () => config.onTap(context),
                 ),
-              ),
-            ),
-            LegalDocumentButton(
-              leadingIcon: Icons.description_outlined,
-              title: s.legalDisclaimerCheckboxRegistrationAgreement,
-              onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => LegalDocumentPage(
-                    params: LegalDocumentParams(
-                      title: s.legalDisclaimerCheckboxRegistrationAgreement,
-                      assetBaseName: 'registration_agreement',
-                      pdfUrls: const {
-                        'de':
-                            'https://realunit.de/wp-content/uploads/dlm_uploads/2025/03/250321_RegV-DE-RealUnit-Schweiz-AG_final_signed.pdf',
-                        'en':
-                            'https://realunit.de/wp-content/uploads/dlm_uploads/2025/03/250321_RegV-EN-RealUnit-Schweiz-AG_final_signed.pdf',
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            LegalDocumentButton(
-              leadingIcon: Icons.article_outlined,
-              title: s.legalDisclaimerCheckboxSecuritiesProspectusBearerShares,
-              onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => LegalDocumentPage(
-                    params: LegalDocumentParams(
-                      title: s.legalDisclaimerCheckboxSecuritiesProspectusBearerShares,
-                      assetBaseName: 'securities_prospectus_bearer_shares',
-                      pdfUrls: const {
-                        'de':
-                            'https://realunit.de/wp-content/uploads/dlm_uploads/2025/07/VO_RealUnit_Wertpapierprospekt_Inhaberaktie_30.06.2025_eIDAS-signiert.pdf',
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            LegalDocumentButton(
-              leadingIcon: Icons.article_outlined,
-              title: s.legalDisclaimerCheckboxSecuritiesProspectusRegisteredShares,
-              onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => LegalDocumentPage(
-                    params: LegalDocumentParams(
-                      title: s.legalDisclaimerCheckboxSecuritiesProspectusRegisteredShares,
-                      assetBaseName: 'securities_prospectus_registered_shares',
-                      pdfUrls: const {
-                        'de':
-                            'https://realunit.de/wp-content/uploads/dlm_uploads/2025/07/VO_RealUnit_Wertpapierprospekt_Namensaktien_30.06.2025_eIDAS-signiert.pdf',
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            LegalDocumentButton(
-              leadingIcon: Icons.account_balance_outlined,
-              title: s.legalDisclaimerCheckboxArticlesOfAssociation,
-              onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => LegalDocumentPage(
-                    params: LegalDocumentParams(
-                      title: s.legalDisclaimerCheckboxArticlesOfAssociation,
-                      assetBaseName: 'articles_of_association',
-                      pdfUrls: const {
-                        'de':
-                            'https://realunit.de/wp-content/uploads/dlm_uploads/2025/06/250604-RUCH-Statuten-mit-Deckblatt.pdf',
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            LegalDocumentButton(
-              leadingIcon: Icons.policy_outlined,
-              title: s.legalDisclaimerCheckboxInvestmentRegulations,
-              onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => LegalDocumentPage(
-                    params: LegalDocumentParams(
-                      title: s.legalDisclaimerCheckboxInvestmentRegulations,
-                      assetBaseName: 'investment_regulations',
-                      pdfUrls: const {
-                        'de':
-                            'https://realunit.de/wp-content/uploads/2025/03/250304_Anlagereglement_RealUnitSchweiz-AG.pdf',
-                      },
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
+              )
+              .toList(),
         ),
       ],
     );
