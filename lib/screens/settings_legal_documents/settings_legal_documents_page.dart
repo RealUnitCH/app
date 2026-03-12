01df4a42ba4b0b212629b6ff68434ea02e838e29
Lam Nguyen <32935491+xlamn@users.noreply.github.com>
2026-03-12 10:17:53 +0100

    feat: move t&c into settings legal documents (#132)

--- lib/screens/settings_legal_documents/settings_legal_documents_page.dart
+++ lib/screens/settings_legal_documents/settings_legal_documents_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:realunit_wallet/generated/i18n.dart';
 import 'package:realunit_wallet/packages/config/legal_documents_config.dart';
 import 'package:realunit_wallet/screens/legal/widgets/legal_document_button.dart';
@@ -14,21 +15,26 @@ class SettingsLegalDocumentsPage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(
+          padding: const .symmetric(
             horizontal: 20.0,
-            vertical: 16.0,
+            vertical: 12.0,
           ),
           child: Column(
             spacing: 12.0,
-            children: LegalDocumentsConfig.allDocuments
-                .map(
-                  (config) => LegalDocumentButton(
-                    leadingIcon: config.icon,
-                    title: config.title(context),
-                    onTap: () => config.onTap(context),
-                  ),
-                )
-                .toList(),
+            children: [
+              LegalDocumentButton(
+                leadingIcon: Icons.description_outlined,
+                title: S.of(context).termsOfUse,
+                onTap: () => context.push('/settings/termsOfUse'),
+              ),
+              ...LegalDocumentsConfig.allDocuments.map(
+                (config) => LegalDocumentButton(
+                  leadingIcon: config.icon,
+                  title: config.title(context),
+                  onTap: () => config.onTap(context),
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- test/screens/settings_legal_documents/settings_legal_documents_page_test.dart
+++ test/screens/settings_legal_documents/settings_legal_documents_page_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:realunit_wallet/packages/config/legal_documents_config.dart';
 import 'package:realunit_wallet/screens/legal/widgets/legal_document_button.dart';
 import 'package:realunit_wallet/screens/settings_legal_documents/settings_legal_documents_page.dart';
 
@@ -11,7 +12,10 @@ void main() {
       await tester.pumpApp(const SettingsLegalDocumentsPage());
 
       expect(find.byType(SingleChildScrollView), findsOne);
-      expect(find.byType(LegalDocumentButton), findsNWidgets(6));
+      expect(
+        find.byType(LegalDocumentButton),
+        findsNWidgets(LegalDocumentsConfig.allDocuments.length + 1),
+      );
     });
   });
 }
