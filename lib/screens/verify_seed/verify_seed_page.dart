e358505ec0b6de11708608a5e14055a0c8ffbc4a
TuanLamNguyen <xnguyen.lam@gmail.com>
2026-03-17 11:57:46 +0100

    feat: show state in button.

--- lib/screens/verify_seed/verify_seed_page.dart
+++ lib/screens/verify_seed/verify_seed_page.dart
@@ -7,6 +7,7 @@ import 'package:realunit_wallet/packages/service/wallet_service.dart';
 import 'package:realunit_wallet/packages/wallet/wallet.dart';
 import 'package:realunit_wallet/screens/home/bloc/home_bloc.dart';
 import 'package:realunit_wallet/screens/verify_seed/cubit/verify_seed_cubit.dart';
+import 'package:realunit_wallet/screens/verify_seed/widgets/verify_seed_button.dart';
 import 'package:realunit_wallet/screens/verify_seed/widgets/verify_seed_input_field.dart';
 import 'package:realunit_wallet/styles/colors.dart';
 
@@ -35,82 +36,69 @@ class VerifySeedView extends StatelessWidget {
     return Scaffold(
       backgroundColor: RealUnitColors.brand700,
       appBar: AppBar(),
-      body: SafeArea(
-        child: Padding(
-          padding: const .symmetric(horizontal: 20),
-          child: BlocConsumer<VerifySeedCubit, VerifySeedState>(
-            listener: (context, state) {
-              if (state.isVerified) {
-                context.read<HomeBloc>().add(
-                  LoadWalletEvent(wallet),
-                );
+      body: Padding(
+        padding: const .symmetric(horizontal: 20),
+        child: BlocConsumer<VerifySeedCubit, VerifySeedState>(
+          listener: (context, state) async {
+            if (state.isVerified) {
+              await Future.delayed(const Duration(seconds: 2));
+              if (context.mounted) {
+                context.read<HomeBloc>().add(LoadWalletEvent(wallet));
               }
-            },
-            builder: (context, state) {
-              if (state.wordIndices.isEmpty) {
-                return const SizedBox.shrink();
-              }
+            }
+          },
+          builder: (context, state) {
+            if (state.wordIndices.isNotEmpty) {
               return LayoutBuilder(
                 builder: (context, constraint) {
                   return SingleChildScrollView(
                     child: ConstrainedBox(
                       constraints: BoxConstraints(minHeight: constraint.maxHeight),
                       child: IntrinsicHeight(
-                        child: Column(
-                          mainAxisAlignment: .start,
-                          children: [
-                            SvgPicture.asset(
-                              'assets/images/illustrations/backup_wallet.svg',
-                              width: 124,
-                            ),
-                            const SizedBox(height: 28),
-                            Column(
-                              spacing: 8.0,
-                              children: [
-                                Text(
-                                  S.of(context).verifySeedTitle,
-                                  style: Theme.of(context).textTheme.headlineMedium,
-                                  textAlign: .center,
-                                ),
-                                Text(
-                                  S.of(context).verifySeedSubtitle,
-                                  textAlign: .center,
-                                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                                    color: RealUnitColors.neutral500,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 40),
-                            VerifySeedInputField(
-                              wordIndices: state.wordIndices,
-                              hasError: state.hasError,
-                            ),
-                            const Spacer(),
-                            Padding(
-                              padding: const .symmetric(vertical: 20),
-                              child: SizedBox(
-                                width: .infinity,
-                                child: FilledButton(
-                                  onPressed: state.canVerify
-                                      ? () => context.read<VerifySeedCubit>().verify()
-                                      : null,
-                                  child: Text(
-                                    S.of(context).confirm,
+                        child: SafeArea(
+                          child: Column(
+                            mainAxisAlignment: .start,
+                            children: [
+                              SvgPicture.asset(
+                                'assets/images/illustrations/backup_wallet.svg',
+                                width: 124,
+                              ),
+                              const SizedBox(height: 28),
+                              Column(
+                                spacing: 8.0,
+                                children: [
+                                  Text(
+                                    S.of(context).verifySeedTitle,
+                                    style: Theme.of(context).textTheme.headlineSmall,
                                     textAlign: .center,
                                   ),
-                                ),
+                                  Text(
+                                    S.of(context).verifySeedSubtitle,
+                                    textAlign: .center,
+                                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                      color: RealUnitColors.neutral500,
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                          ],
+                              const SizedBox(height: 40),
+                              VerifySeedInputField(
+                                wordIndices: state.wordIndices,
+                                hasError: state.hasError,
+                              ),
+                              const Spacer(),
+                              const VerifySeedButton(),
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   );
                 },
               );
-            },
-          ),
+            }
+            return const SizedBox.shrink();
+          },
         ),
       ),
     );
