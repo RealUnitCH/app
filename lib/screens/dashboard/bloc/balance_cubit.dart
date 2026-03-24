82b4de64358dc9caf463aed075b277e09e39706b
TuanLamNguyen <xnguyen.lam@gmail.com>
2026-03-24 10:55:50 +0100

    fix: close subscription in balance cubit to prevent leaks.

--- lib/screens/dashboard/bloc/balance_cubit.dart
+++ lib/screens/dashboard/bloc/balance_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:realunit_wallet/models/asset.dart';
 import 'package:realunit_wallet/models/balance.dart';
@@ -8,16 +10,25 @@ class BalanceCubit extends Cubit<Balance> {
     this._repository, {
     required this.asset,
     required String walletAddress,
-  }) : super(Balance(
-          chainId: asset.chainId,
-          contractAddress: asset.address,
-          walletAddress: walletAddress,
-          balance: BigInt.zero,
-          asset: asset,
-        )) {
-    _repository.watchBalance(state).listen(emit);
+  }) : super(
+         Balance(
+           chainId: asset.chainId,
+           contractAddress: asset.address,
+           walletAddress: walletAddress,
+           balance: BigInt.zero,
+           asset: asset,
+         ),
+       ) {
+    _subscription = _repository.watchBalance(state).listen(emit);
   }
 
   final BalanceRepository _repository;
   final Asset asset;
+  StreamSubscription<Balance>? _subscription;
+
+  @override
+  Future<void> close() {
+    _subscription?.cancel();
+    return super.close();
+  }
 }
