eec8351af4449036c20ae879c6e8b60bcf5eb25f
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2026-04-14 13:49:00 +0200

    Remove dead code from previous cleanup
    - Remove unused extension methods (asShortAddress, asMediumAddress,
      isValidAddress) from ToEpiAddress
    - Remove unused settingsNodes i18n key from ARB files

--- lib/packages/service/transaction_history_service.dart
+++ lib/packages/service/transaction_history_service.dart
@@ -136,26 +136,7 @@ class TransactionHistoryService {
 extension ToEpiAddress on String {
   String get asHexEip55 => EthereumAddress.fromHex(this).hexEip55;
 
-  String get asShortAddress {
-    final address = asHexEip55;
-    return '${address.substring(0, 6)}...${address.substring(address.length - 5)}';
-  }
-
-  bool get isValidAddress {
-    try {
-      EthereumAddress.fromHex(this);
-      return true;
-    } catch (_) {
-      return false;
-    }
-  }
-
   String get asShortTxId {
     return '${substring(0, 10)}...${substring(length - 10)}';
-  }
-
-  String get asMediumAddress {
-    final address = asHexEip55;
-    return '${address.substring(0, 10)}...${address.substring(address.length - 10)}';
   }
 }
