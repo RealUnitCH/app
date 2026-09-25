dff354cc07f1ef6deb11734ae060425058c1e282
TaprootFreakAI <315477232+TaprootFreakAI@users.noreply.github.com>
2026-09-25 15:34:48 +0200

    revert: take the insider switch-off off develop
    The change belongs on staging and should reach develop only through
    the promote. This reverts the merge of #1001.

--- lib/screens/settings/widgets/settings_version_unlock.dart
+++ lib/screens/settings/widgets/settings_version_unlock.dart
@@ -28,9 +28,6 @@ class _SettingsVersionUnlockState extends State<SettingsVersionUnlock> {
       return;
     }
 
-    if (_tapCount >= 7) {
-      _tapCount = 0;
-    }
     _tapCount++;
     if (_tapCount == 7) {
       settingsBloc.add(const UnlockInsiderFeaturesEvent());
